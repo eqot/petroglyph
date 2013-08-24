dfe466547523f761fd2cefe0b5e989837698ff7b
Ikuo Terado <eqobar@gmail.com>
2013-08-25 04:55:28 +0900

    Polished content view

--- app/scripts/controllers/content.js
+++ app/scripts/controllers/content.js
@@ -1,7 +1,18 @@
 'use strict';
 
 angular.module('petroglyphApp')
-  .controller('ContentCtrl', function ($scope, $routeParams) {
+  .controller('ContentCtrl', function ($scope, $routeParams, YouTube) {
+
     var videoId = $routeParams.id;
-    console.log(videoId);
+    // console.log(videoId);
+
+    YouTube.getInfo(videoId, function (res) {
+      // console.log(res);
+      $scope.title = res.data.title;
+      $scope.description = res.data.description;
+      $scope.thumbnail = res.data.thumbnail.sqDefault;
+      $scope.duration = res.data.duration;
+      $scope.durationString = YouTube.getDuration(res.data.duration);
+    });
+
   });
---
+++ app/scripts/services/youtube.js
@@ -0,0 +1,38 @@
+'use strict';
+
+angular.module('petroglyphApp')
+  .service('YouTube', function Youtube($http) {
+
+    return {
+      getVideoId: function (url) {
+        var videoId = null;
+        if (url.match(/^http:\/\/www.youtube.com\/watch\?v=([\w-]+).*/)) {
+          videoId = RegExp.$1;
+        }
+        return videoId;
+      },
+
+      getInfo: function (videoId, callback) {
+        $http.jsonp('http://gdata.youtube.com/feeds/api/videos/' + videoId + '?v=2&alt=jsonc&callback=JSON_CALLBACK')
+          .success(function (res) {
+            if (callback) {
+              callback(res);
+            }
+          });
+      },
+
+      getDuration: function (duration) {
+        if (!duration) {
+          return null;
+        }
+
+        // console.log(scope.duration);
+        var sec = duration % 60;
+        var min = Math.floor(duration / 60) % 60;
+        var hour = Math.floor(duration / 3600);
+        // console.log(hour + ':' + min + ':' + sec);
+        return (hour > 0 ? hour + ':' : '') + min + ':' + (sec < 10 ? '0' + sec : sec);
+      }
+    };
+
+  });
