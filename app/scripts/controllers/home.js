0c7327922eab0dc0ca63a0cb14f76348af947d7a
Ikuo Terado <eqobar@gmail.com>
2013-08-28 00:39:31 +0900

    Supported Google OAuth2

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,6 +1,47 @@
+/*global gapi */
+
 'use strict';
 
 angular.module('petroglyphApp')
   .controller('HomeCtrl', function ($scope) {
+
+    var OAUTH2_CLIENT_ID = 'xxxxxxxx';
+    var OAUTH2_SCOPES = [
+      // 'https://www.googleapis.com/auth/yt-analytics.readonly',
+      'https://www.googleapis.com/auth/youtube.readonly'
+    ];
+
+    gapi.auth.init(function() {
+      setTimeout(checkAuth, 1);
+    });
+
+    function checkAuth () {
+      console.log('checkAuth()');
+
+      gapi.auth.authorize({
+        client_id: OAUTH2_CLIENT_ID,
+        scope: OAUTH2_SCOPES,
+        immediate: true
+      }, handleAuthResult);
+    }
+
+    function handleAuthResult (authResult) {
+      console.log('handleAuthResult()');
+      console.log(authResult);
+
+      if (authResult) {
+        console.log('Authenticated.');
+      } else {
+        console.log('Failed.');
+      }
+    }
+
+    $scope.signin = function () {
+      gapi.auth.authorize({
+        client_id: OAUTH2_CLIENT_ID,
+        scope: OAUTH2_SCOPES,
+        immediate: false
+      }, handleAuthResult);
+    };
 
   });
