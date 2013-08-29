9dbf8f5d43af6c6839ea814d78ec57a884c6a6ef
Ikuo Terado <eqobar@gmail.com>
2013-08-29 20:51:32 +0900

    Added code to load Google APIs

--- app/scripts/controllers/org/home.js
+++ app/scripts/controllers/org/home.js
@@ -31,6 +31,7 @@ angular.module('petroglyphApp')
 
       if (authResult) {
         console.log('Authenticated.');
+        loadAPIClientInterfaces();
       } else {
         console.log('Failed.');
       }
@@ -43,5 +44,34 @@ angular.module('petroglyphApp')
         immediate: false
       }, handleAuthResult);
     };
+
+    function loadAPIClientInterfaces() {
+      gapi.client.load('youtube', 'v3', function() {
+        gapi.client.load('youtubeAnalytics', 'v1', function() {
+          getUserChannel();
+        });
+      });
+    }
+
+    function getUserChannel() {
+      console.log('ok');
+
+      var request = gapi.client.youtube.channels.list({
+        mine: true,
+        part: 'id,contentDetails'
+      });
+
+      request.execute(function(response) {
+        console.log(response);
+        if ('error' in response) {
+          // displayMessage(response.error.message);
+          console.log(response.error.message);
+        } else {
+          // channelId = response.items[0].id;
+          // var uploadsListId = response.items[0].contentDetails.relatedPlaylists.uploads;
+          // getUploadsList(uploadsListId);
+        }
+      });
+    }
 
   });
