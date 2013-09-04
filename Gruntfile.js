b012f43ff16928b71d36debeda6c48135d37046b
Ikuo Terado <eqobar@gmail.com>
2013-09-04 22:33:23 +0900

    Supported watching JS files in modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,6 +57,7 @@ module.exports = function (grunt) {
           '<%= yeoman.app %>/{,*/}*.html',
           '.tmp/styles/{,*/}*.css',
           '{.tmp,<%= yeoman.app %>}/scripts/{,*/}*.js',
+          '<%= yeoman.app %>/modules/**/*.js',
           '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
         ]
       }
